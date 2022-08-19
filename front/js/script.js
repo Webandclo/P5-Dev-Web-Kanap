649754a461b3e890d62f1741ca4f74fa3182c8fa
Webandclo <claudie.jousse@gmail.com>
2022-08-19 15:44:03 +0200

    On récupère les produits de l'API

--- front/js/script.js
+++ front/js/script.js
@@ -1,61 +1,27 @@
 // Récupérer les produits de l'API
-function AllProducts() {
-    fetch("http://localhost:3000/api/products")
-    .then(function(res) {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .catch(function(err) {
-        // Une erreur est survenue
-        return error;
-      })
+fetch("http://localhost:3000/api/products/")
+    .then(data => data.json())
+
+
+// Création de chaque éléments avec interpolation de variable
+.then(jsonListProduct => {
+    let contenu = "";
+
+    //On boucle sur chaque produit et on construit les cards en HTML
+    for (let jsonProduct of jsonListProduct) {
+
+        contenu += ` <a href="./product.html?id=${jsonProduct._id}">
+                    <article>
+                    <img src="${jsonProduct.imageUrl}" alt="${jsonProduct.altTxt}">
+                    <h3 class="productName">${jsonProduct.name}</h3>
+                    <p class="productDescription">${jsonProduct.description}</p>
+                    </article>
+                </a> `
     }
 
+    // Selectionner dans le DOM
+    let sectionIems = document.querySelector("#items")
 
-
-
-
-
-// Afficher dans le DOM
-
-
-// for (let i = 0; i < products.length; i++) {
-        // Ceci on compte le nombre de produits
-        // À chaque éxécution, la variable "products" augmentera de 1
-        // Lorsque'elle aura affiché tous les produits, le boucle se terminera.
-    //code
-
-    //console.log(products);
-// }
-
-
-let product = document.querySelector("#items");
-
-let productA = document.createElement("a");
-product.appendChild(productA);
-
-let productArticle = document.createElement("article");
-productA.appendChild(productArticle);
-
-let productImg = document.createElement("img");
-productArticle.appendChild(productImg);
-productImg.src = "product01.jpg";
-
-let productH3 = document.createElement("h3");
-productArticle.appendChild(productH3);
-productH3.innerText = "Kanap name1";
-productH3.classList.add("productName");
-
-let productP = document.createElement("p");
-productArticle.appendChild(productP);
-productP.classList.add("productDescription");
-productP.innerText = "Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.";
-
-
-
-
-
-
-console.log(product);
-
+    // Afficher dans le DOM
+    sectionIems.innerHTML = contenu;
+});
