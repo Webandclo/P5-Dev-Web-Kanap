8b0eaff49cbe6213d24daef8a886b1bace95cd52
Webandclo <claudie.jousse@gmail.com>
2022-08-23 16:32:18 +0200

    affichage des éléments page product

--- front/js/product.js
+++ front/js/product.js
@@ -21,45 +21,81 @@ if(search_params.has('id')) {
 const productColor = document.querySelector ('#colors');
 const productQuantity = document.querySelector('#quantity');
 
+///
+  // Requête de l'API
+  async function getProduct() {
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/products/" + idProduct
+      );
 
-//on va chercher l'API avec la methode fetch et on ajoute notre variable qui contient l'id
-let product;
-async function getProduct() {
-    await fetch("http://localhost:3000/api/products/" + idProduct)
-    .then(data => data.json()
-    .then(json => product = json))
-    //console.log("product1", product)
+      const responseProduct = await response.json();
 
-    .catch((error) => {
-        console.log("Erreur de la requête API");
-    })
+      //console.log("responseProduct", responseProduct)
+      //affichageInfoByCreateElement(responseProduct);
+      affichageInfoByInnerHTML(responseProduct)
+
+
+    } catch (error) {
+      console.log(error.message);
+    }
     // .catch(function(err) {
     //   // Une erreur est survenue
     //   return error;
     // })
-};
-getProduct(); // On appelle la fonction
+  }
+  
+  
+  getProduct(); // On appelle la fonction
+///
+
 
 
 // Selectionner dans le DOM
 
 function getPost(product){
-      //ajout image 
+      //Ajouter image 
       let productImage = document.createElement("img");
       document.querySelector(".item__img").appendChild(productImage);
       productImage.src = product.imageUrl;
       productImage.alt = product.altTxt;
 }
 
-getPost(product); // On appelle la fonction
+//getPost(product); // On appelle la fonction
 
+////
 
+// Récupération des infos des produits
+function affichageInfoByCreateElement(product) {
+  let imgContainer = document.querySelector(".item__img");
+  let productImg = document.createElement("img");
+  productImg.src = product.imageUrl;
+  productImg.alt = product.altTxt;
+  imgContainer.appendChild(productImg);
 
+  let productTitle = document.getElementById("title");
+  productTitle.innerText = product.name;
 
+  let productPrice = document.getElementById("price");
+  productPrice.innerHTML = product.price;
 
-/// Teste à effacer ///
+  let productDescription = document.getElementById("description");
+  productDescription.innerText = product.description;
 
+  let colorId = document.getElementById("colors");
+  let colorArray = product.colors;
+  for (let color of colorArray) {
+    colorId.innerHTML += `<option value="${color}"> ${color}</option>`;
+  }
+}
 
+function affichageInfoByInnerHTML(product) {
+  document.querySelector(".item__img").insertAdjacentHTML("afterbegin", `<img src="${product.imageUrl}" alt="${product.altTxt}">`);
+  document.querySelector("#title").insertAdjacentHTML("afterbegin", `${product.name}`);
+  document.querySelector("#price").insertAdjacentHTML("afterbegin", `${product.price}`);
+  document.querySelector("#description").insertAdjacentHTML("afterbegin", `${product.description}`);
+  for (let productSelectColor of product.colors) {
+    document.querySelector("#colors").innerHTML += `<option value="${productSelectColor}">${productSelectColor}</option>`
+  };
 
-
-/// Fin test ///
+}
